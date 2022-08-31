058da7b49433bb7c004631da49f3fc501ad6fdad
Lucas Pereira <lucaspereiracampos974@gmail.com>
2022-08-31 20:57:07 -0300

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Buscador CEP</h1>
+    <div className="container">
+      <h1 classname="title">Buscador CEP</h1>
+
+      <div classname="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu CEP"
+        />
+      </div> 
+
+      <button classname="buttonSearch">
+        Procurar
+      </button>
+
+
+
     </div>
   );
 }
